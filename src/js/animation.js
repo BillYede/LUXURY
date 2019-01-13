e0aa4f3fe216ca43cac9d8cbd2a92f90798471c0
Bill Wilson Yede Nka <bill9414@hotmail.com>
2019-01-13 05:05:39 +0100

    big changes

---
+++ src/js/animation.js
@@ -0,0 +1,31 @@
+const animate = document.querySelectorAll(".building, .investment, .estimate, .offers__cards, .history");
+
+animate.forEach(element => {
+    element.style.opacity = 0
+});
+
+const fadeIn = function () {
+
+
+    let delay = 0.25;
+
+    animate.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top
+        const elementBottom = element.getBoundingClientRect().bottom
+
+        if(elementTop < window.innerHeight) {
+            element.style.animation = `fadein 1s ${delay}s both`
+            delay = delay + 0.25;
+        }
+    })
+}
+fadeIn();
+
+document.addEventListener("scroll", function (){
+    fadeIn()
+})
+
+window.addEventListener("resize", function(){
+    fadeIn()
+})
+
